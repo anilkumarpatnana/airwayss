e87cddfb2dbd056f6a1b34a1766aa085638e7ba3
Anil <Anilkumarpatnana555@gmail.com>
2025-04-28 22:27:53 +0530

    changes made

--- src/landing-page/Services.jsx
+++ src/landing-page/Services.jsx
@@ -7,10 +7,10 @@ import React from 'react'
       <div className='services'>
       <div className='service1'>
         <img src="./images/travelwithfreshness.png" alt="" />
-      <p>Travel With All The Freshness</p>
+      <p className='captions'>Travel With All The Freshness</p>
       </div>
-      <div className='service2'><img src="./images/customercare.jpg" alt="" /><p>We Will Give 24/7 Support, Always With You</p></div>
-      <div className='service3'><img src="./images/budgetfriendly.jpg" alt="" /><p>Travel With Affordable Prices, Budget Friendly</p></div>
+      <div className='service2'><img src="./images/customercare.jpg" alt="" /><p className='captions'>We Will Give 24/7 Support, Always With You</p></div>
+      <div className='service3'><img src="./images/budgetfriendly.jpg" alt="" /><p className='captions'>Travel With Affordable Prices, Budget Friendly</p></div>
       </div>
     </div>
 
